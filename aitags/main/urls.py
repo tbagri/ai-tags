ce82a0a91f7a427a97e4b42ae17c73394515e6d1
bharatmekala <bharatmekala5@gmail.com>
2024-02-25 12:22:04 -0800

    updating

--- aitags/main/urls.py
+++ aitags/main/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # 👈 Add include here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 👇 add your myapi app urls path here
+    path('api/', include('myapi.urls'))
 ]
